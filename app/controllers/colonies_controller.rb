ce3d177b112797f090ca7ea48a15665a2398b47a
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-20 11:30:31 +0900

    pundit and devise setup

--- app/controllers/colonies_controller.rb
+++ app/controllers/colonies_controller.rb
@@ -1,4 +1,18 @@
 class ColoniesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
+    @colonies = Colony.all
+  end
+
+  private
+
+  def colony_params
+    params.require(:colony).permit(:name, :location, :description, :radius)
+  end
+
+  def set_colony
+    @colony = Colony.find(params[:id])
+    authorize @colony
   end
 end
