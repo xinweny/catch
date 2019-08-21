eae5340073c761b299605b6424af645d2d076dee
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-21 11:00:29 +0900

    radius added

--- app/controllers/colonies_controller.rb
+++ app/controllers/colonies_controller.rb
@@ -10,6 +10,7 @@ class ColoniesController < ApplicationController
       {
         lat: colony.latitude,
         lng: colony.longitude,
+        radius: colony.radius,
         infoWindow: { content: render_to_string(partial: "/colonies/info_window", locals: { colony: colony }) }
         # image_url: helpers.asset_url(‘file in the assets/images folder’)
       }
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -13,4 +13,18 @@ if (mapElement) {
   } else {
     map.fitLatLngBounds(markers);
   }
+
+  markers.forEach((marker) => {
+    console.log(map);
+    let colonyRadius = new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map.map,
+      center: { lat: marker.lat, lng: marker.lng },
+      radius: marker.radius
+    });
+  });
 }
